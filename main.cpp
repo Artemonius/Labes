20286d5d216ffd50e55bf8f839e0f542b07050d7
Artemonius <73630737+Artemonius@users.noreply.github.com>
2021-05-31 13:47:47 +0500

    lab 20  binar tree

--- main.cpp
+++ main.cpp
@@ -1,22 +1,11 @@
-#include <iostream>
-#include <fstream>
-#include "lab18.h"
-#include <string>
-#include <windows.h>
-#include <vector>
-#include <list>
-#include <queue>
-#include <stack>
-#include <set>
-#include <map>
-#include <algorithm>
-#include <numeric>
-using namespace std;
-int main()
+#include "mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
 {
-    SetConsoleCP(1251);
-    SetConsoleOutputCP(1251);
-    Vector8 V;
-    Dialog D;
-    D.Execute();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
